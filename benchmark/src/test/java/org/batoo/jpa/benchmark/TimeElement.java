ad02bedcc8380721e6330bbebedda8780b91ec3f
hceylan <hceylan@ceylan.batoo.org>
2012-09-26 00:33:02 +0300

    Benchmark updated

--- benchmark/src/test/java/org/batoo/jpa/benchmark/TimeElement.java
+++ benchmark/src/test/java/org/batoo/jpa/benchmark/TimeElement.java
@@ -95,7 +95,7 @@ public class TimeElement extends HashMap<String, TimeElement> implements Compara
 	 * @author hceylan
 	 */
 	public int dump(int rowNo, int depth) {
-		if (depth > 0 && this.timeWithoutDerby > 10000000) {
+		if ((depth > 0) && (this.timeWithoutDerby > 10000000)) {
 			rowNo++;
 			final String tabs = StringUtils.repeat(" ", depth);
 			System.out.println(String.format("%010d", rowNo) + //
