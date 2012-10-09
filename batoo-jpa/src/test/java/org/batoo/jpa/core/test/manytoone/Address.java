a583c3c9e338671409db33147c890bab016302b5
asimarslan <arslanasim@gmail.com>
2012-10-09 22:26:45 +0300

    joinCoulumn and count test

---
+++ batoo-jpa/src/test/java/org/batoo/jpa/core/test/manytoone/Address.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2012 - Batoo Software ve Consultancy Ltd.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.jpa.core.test.manytoone;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * 
+ * @author hceylan
+ * @since $version
+ */
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+
+	private String city;
+
+	/**
+	 * @since $version
+	 * @author hceylan
+	 */
+	public Address() {
+		super();
+	}
+
+	/**
+	 * @param person
+	 *            the person
+	 * @param city
+	 *            the city
+	 * 
+	 * @since $version
+	 * @author hceylan
+	 */
+	public Address(String city) {
+		super();
+		this.city = city;
+	}
+
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
