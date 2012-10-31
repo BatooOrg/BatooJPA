610f63a81b45774908b3b39b796af1d1dbdca219
hceylan <hceylan@localhost>
2012-10-31 18:22:23 +0200

    Added copyright.

--- community/src/test/java/org/batoo/jpa/community/test/t1/Parameter.java
+++ community/src/test/java/org/batoo/jpa/community/test/t1/Parameter.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2012 - Batoo Software import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.batoo.jpa.community.test.t1;
 
 import java.io.Serializable;
