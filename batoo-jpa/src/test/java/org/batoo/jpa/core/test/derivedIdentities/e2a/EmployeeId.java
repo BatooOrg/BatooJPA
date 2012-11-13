e55c10f51915ff0bc691ce82c262403ad555e082
asimarslan <arslanasim@gmail.com>
2012-11-14 00:50:39 +0200

    derived id test spec example 2a

---
+++ batoo-jpa/src/test/java/org/batoo/jpa/core/test/derivedIdentities/e2a/EmployeeId.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2012 - Batoo Software ve Consultancy Ltd.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.jpa.core.test.derivedIdentities.e2a;
+
+/**
+ * 
+ * @author asimarslan
+ * @since $version
+ */
+public class EmployeeId {
+	String firstName;
+	String lastName;
+
+	/**
+	 * 
+	 * @author asimarslan
+	 * @since $version
+	 */
+	public EmployeeId() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @author asimarslan
+	 * @since $version
+	 * @param firstName
+	 * @param lastName
+	 */
+	public EmployeeId(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final EmployeeId other = (EmployeeId) obj;
+		if (this.firstName == null) {
+			if (other.firstName != null) {
+				return false;
+			}
+		}
+		else if (!this.firstName.equals(other.firstName)) {
+			return false;
+		}
+		if (this.lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		}
+		else if (!this.lastName.equals(other.lastName)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 
+	 * @return the firstName
+	 * 
+	 * @author asimarslan
+	 * @since $version
+	 */
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	/**
+	 * 
+	 * @return the lastName
+	 * 
+	 * @author asimarslan
+	 * @since $version
+	 */
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.firstName == null) ? 0 : this.firstName.hashCode());
+		result = prime * result + ((this.lastName == null) ? 0 : this.lastName.hashCode());
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param firstName
+	 *            the firstName to set
+	 * 
+	 * @author asimarslan
+	 * @since $version
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * 
+	 * @param lastName
+	 *            the lastName to set
+	 * 
+	 * @author asimarslan
+	 * @since $version
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
