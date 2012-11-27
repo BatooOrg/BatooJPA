15594a216fdc244f63c49e9827b6cc010214b0df
Hasan Ceylan <hceylan@batoo.org>
2012-11-27 15:06:54 +0200

    Test case: weird behavior of persist in JTA mode
    https://github.com/BatooOrg/BatooJPA/issues/108

---
+++ community/src/test/java/org/batoo/jpa/community/test/i108/Manager.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2012 - Batoo Software ve Consultancy Ltd.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.jpa.community.test.i108;
+
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Singleton
+@SuppressWarnings("javadoc")
+public class Manager {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public void persist() {
+		this.em.persist(new Batoo());
+	}
+}
---
+++ community/src/test/java/org/batoo/jpa/community/test/i108/TestIssue108.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2012 - Batoo Software ve Consultancy Ltd.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.jpa.community.test.i108;
+
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.openejb.jee.EnterpriseBean;
+import org.apache.openejb.jee.SingletonBean;
+import org.apache.openejb.jee.jpa.unit.PersistenceUnit;
+import org.apache.openejb.junit.ApplicationComposer;
+import org.apache.openejb.junit.Module;
+import org.batoo.jpa.core.BatooPersistenceProvider;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ApplicationComposer.class)
+@SuppressWarnings("javadoc")
+@Ignore
+public class TestIssue108 {
+
+	@PersistenceContext
+	EntityManager em;
+
+	@EJB
+	Manager mgr;
+
+	@Module
+	public EnterpriseBean bean() {
+		return new SingletonBean(Manager.class).localBean();
+	}
+
+	@Test
+	public void go() {
+		this.mgr.persist();
+		this.em.createQuery("select e from Batoo e").getSingleResult();
+	}
+
+	@Module
+	public PersistenceUnit jpa() {
+		final PersistenceUnit u = new PersistenceUnit();
+
+		u.setProvider(BatooPersistenceProvider.class);
+		u.setName("d");
+		u.setProperty("org.batoo.jpa.ddl", "drop");
+		u.setProperty("org.batoo.jpa.sql_logging", "STDOUT");
+		u.getClazz().add(Batoo.class.getName());
+
+		return u;
+	}
+}
