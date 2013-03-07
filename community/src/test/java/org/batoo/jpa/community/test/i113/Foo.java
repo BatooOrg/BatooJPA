6c248e1d3b7af4b953811ee61c4571cdc4c42427
asimarslan <arslanasim@gmail.com>
2013-03-07 21:42:02 +0200

    simple community test for issue 113

---
+++ community/src/test/java/org/batoo/jpa/community/test/i113/Foo.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2012 - Batoo Software ve Consultancy Ltd.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.jpa.community.test.i113;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * 
+ * @author hceylan
+ * @since $version
+ */
+@Entity
+public class Foo {
+
+	@Id
+	private Integer id;
+
+	@Basic
+	@Column(name = "lastActivity", nullable = true)
+	private java.util.Date lastActivity;
+
+	/**
+	 * 
+	 * @return the id
+	 * 
+	 * @since $version
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * 
+	 * @return the lastActivity
+	 * 
+	 * @since $version
+	 */
+	public java.util.Date getLastActivity() {
+		return this.lastActivity;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 *            the id to set
+	 * 
+	 * @since $version
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 * @param lastActivity
+	 *            the lastActivity to set
+	 * 
+	 * @since $version
+	 */
+	public void setLastActivity(java.util.Date lastActivity) {
+		this.lastActivity = lastActivity;
+	}
+
+}
