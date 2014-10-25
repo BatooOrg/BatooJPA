818b5dceb6052c9a0a540aeb4b569e385f00165d
Asım Arslan <arslanasim@gmail.com>
2014-10-25 23:09:55 +0300

    gitignore pom fix

---
+++ batoo-jpa/src/main/java/org/batoo/common/util/LangUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2012-2013, Batu Alp Ceylan
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.common.util;
+
+/**
+ * Language helpers
+ */
+public final class LangUtils {
+
+    private LangUtils() {
+    }
+
+    public static void defaultEnumCase(Enum e) {
+        throw new IllegalStateException("Unsupported enum case" + e);
+    }
+}
